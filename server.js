612c5454b5d8095e439a8290d880f44ec15e955d
MeanBordin <bodinsak2544@gmail.com>
2024-03-05 04:38:29 +0700

    up

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/api/feedback", function (req, res) {
+  res.json({"test": "test"});
 });
 
 const PORT = process.env.PORT || 8080;
